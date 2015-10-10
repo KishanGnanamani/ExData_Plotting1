4fd8c3acbf5552f4f67a1cb0b75d3adcdb76791e
Kishan Gnanamani <kishan.gnanamani@outlook.com>
2015-10-10 03:13:51 -0700

    Create Plot3.R
    Created Plot3.R file

---
+++ Plot3.R
@@ -0,0 +1,34 @@
+# This function takes the complete file path of the household_power_consumption.txt file as input parameter and creates the "Plot3.png" in the working directory
+# Function invocation example - createPlot3("C:/Users/.../household_power_consumption.txt")
+
+createPlot3 <- function(filepath) {
+    
+    fpath <- filepath
+    
+    # reads the file and stores the data in the "pjct1data" data frame
+    pjct1data <- read.table(fpath,sep = ";",header = TRUE)
+    
+    # "ssdata" data frame is created by subsetting only the 02/01/07 and 02/02/07 data from the "pjct1data" data frame
+    ssdata <- subset(pjct1data,pjct1data$Date == "1/2/2007" | pjct1data$Date == "2/2/2007")
+    
+    # new column "DateTime" is created in the "ssdata" data frame bt concatenating the "Date" and " Time" columns
+    ssdata <- cbind(DateTime = paste(as.character(ssdata$Date),as.character(ssdata$Time)),ssdata)
+    
+    # converts the "Global_active_power","Sub_metering_1","Sub_metering_2","Sub_metering_3" columns as numeric fields and the "DateTime" column as a POSIXCT field
+    ssdata$Global_active_power = as.numeric(as.character(ssdata$Global_active_power))
+    ssdata$DateTime = as.POSIXct(ssdata$DateTime,format = "%d/%m/%Y %H:%M:%S")
+    ssdata$Sub_metering_1 = as.numeric(as.character(ssdata$Sub_metering_1))
+    ssdata$Sub_metering_2 = as.numeric(as.character(ssdata$Sub_metering_2))
+    ssdata$Sub_metering_3 = as.numeric(as.character(ssdata$Sub_metering_3))
+    
+    #png graphic file creation with the scatter plot
+    png(file = "Plot3.png",width = 480, height = 480, units = "px")
+    with(ssdata,plot(DateTime,Sub_metering_1,ylim = range(c(Sub_metering_1,Sub_metering_2,Sub_metering_3)),type = "l",xlab = "",ylab = ""))
+    par(new = TRUE)
+    with(ssdata,plot(DateTime,Sub_metering_2,ylim = range(c(Sub_metering_1,Sub_metering_2,Sub_metering_3)),col = "red",type = "l",xlab = "",ylab = ""))
+    par(new = TRUE)
+    with(ssdata,plot(DateTime,Sub_metering_3,ylim = range(c(Sub_metering_1,Sub_metering_2,Sub_metering_3)),col = "blue",type = "l",xlab = "",ylab = "Energy sub metering"))
+    legend("topright",lty = c(1,1,1), col = c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+    dev.off()
+    
+}
